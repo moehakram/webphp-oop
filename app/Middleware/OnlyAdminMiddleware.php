b34c0365d885e167bfd50dc33563701a85be15f2
moehakram <muh.akram1422@gmail.com>
2024-07-10 20:30:37 +0800

    Refactoring code middleware

---
+++ app/Middleware/OnlyAdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Middleware;
+
+use MA\PHPQUICK\Interfaces\Middleware;
+use MA\PHPQUICK\Interfaces\Request;
+
+class OnlyAdminMiddleware implements Middleware
+{
+    public function execute(Request $request, callable $next)
+    {
+        $user = $request->user();
+
+        if ($this->isAdmin($user)) {
+            return $next($request);
+        }
+        return response()->setForbidden();
+    }
+
+    private function isAdmin($user): bool
+    {
+        return $user !== null && $user->getRole() == 1;
+    }
+}
